951de8ec66a0e8c6e62b32bde83c706394cd6552
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-17 00:36:11 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import taravel
+version = taravel.__version__
+
+setup(
+    name='bliski_publikator',
+    version=version,
+    author='Adam Dobrawy',
+    author_email='naczelnik@jawnosc.tk',
+    packages=[
+        'bliski_publikator',
+    ],
+    include_package_data=True,
+    url='https://github.com/watchdogpolska/bliski_publikator',
+    install_requires=[
+        'Django>=1.7.10',
+    ],
+    zip_safe=False,
+    scripts=['manage.py'],
+)
