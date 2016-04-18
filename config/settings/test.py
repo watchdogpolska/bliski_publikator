0a588651fedcedc4eb3a4505ae00bf2abfe33eee
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-04-19 00:22:49 +0200

    Enable template debugging

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,3 +1,5 @@
 from .common import *  # noqa
 
 SECRET_KEY = env("DJANGO_SECRET_KEY", default='=&csx-wb@tf7)u=7g9kkca-3t_nm%j&b^**2&py^@yrn-e#fv4')
+DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
