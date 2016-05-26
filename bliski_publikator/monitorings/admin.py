282b38589aca75c4770dd33b4692aaa37adce09b
Dzakus <Dzakus13@gmail.com>
2016-05-26 11:38:29 +0200

    Add TinyMCE to monitoring's admin

--- bliski_publikator/monitorings/admin.py
+++ bliski_publikator/monitorings/admin.py
@@ -1,12 +1,23 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Monitoring
+from tinymce.widgets import TinyMCE
+
+
+class MonitoringForm(forms.ModelForm):
+
+    class Meta:
+        widgets = {
+            'description': TinyMCE(attrs={'cols': 80, 'rows': 30}),
+        }
 
 
 class MonitoringAdmin(admin.ModelAdmin):
     '''
         Admin View for Monitoring
     '''
+    form = MonitoringForm
     list_display = ('name', 'active')
     list_filter = ('active',)
     search_fields = ('name', 'description')
