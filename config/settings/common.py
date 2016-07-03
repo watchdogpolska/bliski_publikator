0d6b90eeca5b5bb15d1bda5270b679f85a4b22e6
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-07-03 23:43:25 +0200

    Add authentication class to django-rest-framework

--- config/settings/common.py
+++ config/settings/common.py
@@ -257,6 +257,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
