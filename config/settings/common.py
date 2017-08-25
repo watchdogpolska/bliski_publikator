7fcbf490a264eb21ba56b60a5c65885c5617ca69
mik-laj <kamil.bregula@siecobywatelska.pl>
2017-08-26 00:27:18 +0200

    Remove .env variables

--- config/settings/common.py
+++ config/settings/common.py
@@ -16,7 +16,7 @@ ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
 APPS_DIR = ROOT_DIR.path('bliski_publikator')
 
 env = environ.Env()
-environ.Env.read_env()
+#environ.Env.read_env()
 
 # APP CONFIGURATION
 # ------------------------------------------------------------------------------
