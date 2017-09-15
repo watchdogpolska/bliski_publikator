76dc0f2ee222e97f31488d701bc61effbb136f82
Kamil Breguła <mik-laj@users.noreply.github.com>
2017-09-15 23:21:49 +0200

    Fix #37 Replace django-secure with django mechanism (#108)

--- config/settings/common.py
+++ config/settings/common.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # MIDDLEWARE CONFIGURATION
 # ------------------------------------------------------------------------------
 MIDDLEWARE_CLASSES = (
-    # Make sure djangosecure.middleware.SecurityMiddleware is listed first
+    # Make sure django.middleware.security.SecurityMiddleware is listed first
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
